d86e7108b6dac72a425fae0bb104b9b833083e2a
Arjun Judge <arjunsjudge@gmail.com>
2023-10-15 23:33:06 +0100

    initial commits
    adding read me, fifo.h, testfifo.cpp and gitattributes to repo

---
+++ fifo.h
@@ -0,0 +1,136 @@
+#pragma once
+#include <iostream>
+
+// FIFO with circular buffer
+#define MAX_FIFO_SIZE 7
+#define FIFO_BUFFER_SIZE (MAX_FIFO_SIZE + 1)
+
+template <class T>
+class arrayFifo {
+public:
+
+	arrayFifo(uint32_t rp = 0, uint32_t wp = 0)
+	{
+		mWritePtr = wp;
+		mReadPtr = rp;
+	};
+	~arrayFifo() {};
+
+	void decrementWritePointer(void)
+	{
+		mWritePtr--;
+		mWritePtr = (mWritePtr > MAX_FIFO_SIZE) ? MAX_FIFO_SIZE : mWritePtr;
+	}
+	
+	void incrementWritePointer(void)
+	{
+		mWritePtr++; // pointer now points to the next empty element in mBuff
+		mWritePtr = (mWritePtr > MAX_FIFO_SIZE) ? 0 : mWritePtr;
+	}
+	
+	void decrementReadPointer(void)
+	{
+		mReadPtr--; // mReadPtr is an unsigned int so it cannot become negative
+		mReadPtr = (mReadPtr > MAX_FIFO_SIZE) ? MAX_FIFO_SIZE : mReadPtr;
+	}
+	
+	void incrementReadPointer(void)
+	{
+		mReadPtr++;
+		mReadPtr = (mReadPtr > MAX_FIFO_SIZE) ? 0 : mReadPtr;
+	}
+
+	void push_front(T val, bool force = false)	// push new value to the front of the queue
+	{
+		if (!isFull())  // if not full
+		{
+			decrementReadPointer();
+			mBuff[mReadPtr] = val;
+		}
+		else
+		{
+			if (force == true)
+			{
+				mBuff[mWritePtr] = val;
+				decrementReadPointer();
+				decrementWritePointer();
+			}
+		}
+	}
+
+	void push_back(T val, bool force = false)	// Push new value to the back of the queue
+	{
+		if (!isFull())
+		{
+			mBuff[mWritePtr] = val; // the mWritePtrth index of mBuff becomes this val (writing to the end of the fifo)
+			incrementWritePointer();
+		}
+		else
+		{
+			if (force == true)
+			{
+				mBuff[mWritePtr] = val;
+				incrementReadPointer();
+				incrementWritePointer();
+			}
+		}
+	}
+
+	T pop_front()
+	{
+		uint32_t val = 0;
+		if (!isEmpty()) // this pointer points to the size member of the object
+		{
+			val = mBuff[mReadPtr];
+			incrementReadPointer();
+		}
+		return val; // returns the value of the front element
+	}
+
+	T pop_back()
+	{
+		uint32_t val = 0;
+
+		if (!isEmpty())
+		{
+			decrementWritePointer();
+			val = mBuff[mWritePtr];
+		}
+		return val;
+	}
+
+	void clear(void)
+	{
+		mWritePtr = 0;
+		mReadPtr = 0;
+	}
+
+	bool isEmpty(void) { return (mWritePtr == mReadPtr) ? true : false; }
+
+	bool isFull(void) { return (MAX_FIFO_SIZE == size()); }
+
+	uint32_t size()
+	{
+		if (mWritePtr == mReadPtr)
+			return 0;
+		if (mWritePtr > mReadPtr)
+			return (mWritePtr - mReadPtr);
+		else
+			return (uint32_t)(sizeof(mBuff) / sizeof(T)) - (mReadPtr - mWritePtr);
+	}
+
+	uint32_t getWPtr()
+	{
+		return mWritePtr;
+	}
+
+	uint32_t getRPtr()
+	{
+		return mReadPtr;
+	}
+
+private:
+	T mBuff[FIFO_BUFFER_SIZE] = {};
+	uint32_t mWritePtr = 0;
+	uint32_t mReadPtr = 0;
+};
---
+++ testFifo.cpp
@@ -0,0 +1,30 @@
+#include "fifo.h"
+using namespace std;
+
+int main()
+{
+
+	arrayFifo<uint8_t> my_fifo(3, 3);  // initialises read pointer and write pointer to the 3rd element in the array
+
+	for (uint32_t i = 0; i < MAX_FIFO_SIZE; i++)
+	{
+		cout << "current count = " << uint32_t(my_fifo.size()) << endl;
+		cout << "read pointer position = " << uint32_t(my_fifo.getRPtr()) << endl;
+		cout << "write pointer position = " << uint32_t(my_fifo.getWPtr()) << endl;
+		my_fifo.push_back(uint8_t(i));
+	}
+	cout << "final count = " << uint32_t(my_fifo.size()) << endl;
+	cout << "final array full = " << my_fifo.isFull() << endl;
+	cout << endl;
+
+	for (uint32_t i = 0; i < MAX_FIFO_SIZE + 1; i++)
+	{
+		cout << "read pointer position = " << uint32_t(my_fifo.getRPtr()) << endl;
+		cout << "write pointer position = " << uint32_t(my_fifo.getWPtr()) << endl;
+		my_fifo.incrementReadPointer();
+	}
+	cout << "array size should now be 0 as rPtr should equal wPtr " << endl;
+	cout << "final count = " << uint32_t(my_fifo.size()) << endl;
+	bool empty = true ? uint32_t(my_fifo.size()) == 0 : false;
+	cout << empty << endl;
+}
