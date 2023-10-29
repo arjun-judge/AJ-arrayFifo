b5960003d114a19e830a1d9ce7067d3d2cb5464f
arjun-judge <147245002+arjun-judge@users.noreply.github.com>
2023-10-29 12:18:15 +0000

    Update testFifo.cpp
    minor changes - changed "array" with fifo in testFifo.cpp
    corrected final test so that it actually empties the dequeue

--- testFifo.cpp
+++ testFifo.cpp
@@ -8,24 +8,27 @@ int main()
 
 	for (int i = 0; i < MAX_FIFO_SIZE; i++)
 	{
-		cout << "current count = " << uint32_t(my_fifo.size()) << endl;
-		cout << "read pointer position = " << uint32_t(my_fifo.getRPtr()) << endl;
-		cout << "write pointer position = " << uint32_t(my_fifo.getWPtr()) << endl;
+		cout << "current count = " << my_fifo.size() << endl;
+		cout << "read pointer position = " << my_fifo.getRPtr() << endl;
+		cout << "write pointer position = " << my_fifo.getWPtr() << endl;
 		my_fifo.push_back(uint8_t(i));
 	}
-	cout << "final count = " << uint32_t(my_fifo.size()) << endl;
-	cout << "final array full = " << my_fifo.isFull() << endl;
+	cout << "final count = " << my_fifo.size() << endl;
+	cout << "final fifo full = " << my_fifo.isFull() << endl;
 	cout << endl;
 
-	for (int i = 0; i < MAX_FIFO_SIZE + 1; i++)
+	for (int i = 0; i < MAX_FIFO_SIZE; i++)
 	{
-		cout << "read pointer position = " << uint32_t(my_fifo.getRPtr()) << endl;
-		cout << "write pointer position = " << uint32_t(my_fifo.getWPtr()) << endl;
+		cout << "read pointer position = " << my_fifo.getRPtr() << endl;
+		cout << "write pointer position = " << my_fifo.getWPtr() << endl;
 		my_fifo.incrementReadPointer();
 	}
-	cout << "array size should now be 0 as rPtr should equal wPtr " << endl;
-	cout << "final count = " << uint32_t(my_fifo.size()) << endl;
-	bool empty = true ? uint32_t(my_fifo.size()) == 0 : false;
-	cout << empty << endl;
+	cout << "fifo size should now be 0 as rPtr should equal wPtr " << endl;
+	cout << "final count = " << my_fifo.size() << endl;
+	bool empty = true ? my_fifo.size() == 0 : false;
+	cout << "final fifo empty = " << empty << endl;
+	cout << "if I increment the read pointer by 1 once more, the fifo should be full" << endl;
+	my_fifo.incrementReadPointer();
+	cout << "final fifo full = " << my_fifo.isFull() << endl;
 
 }
